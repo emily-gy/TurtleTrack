1f5ad5b058b86b63a0610e96aae69177e38385f6
emily.gy <emily.yang@duke.edu>
2025-10-14 17:18:16 -0400

    iterate all read data and process

--- ARGOSTrackingTool.py
+++ ARGOSTrackingTool.py
@@ -36,3 +36,36 @@ obs_lon = lineData[7]
 
 #Print the location of sara
 print(f"Record {record_id} indicates Sara was seen at lat:{obs_lat},lon:{obs_lon} on {obs_date}")
+
+
+# For loop
+#Create a variable pointing to the data file
+file_name = './data/raw/sara.txt'
+
+#Create a file object from the file
+file_object = open(file_name,'r')
+
+#Read contents of file into a list
+line_list = file_object.readlines()
+
+#Close the file
+file_object.close()
+
+#Read contents of file into a list
+for lineString in line_list:
+    # check if the line is a data line
+    if lineString[0] in ("#", "u"):
+        continue
+
+    #Split the string into a list of data items
+    lineData = lineString.split()
+
+    #Extract items in list into variables
+    record_id = lineData[0]
+    obs_date = lineData[2]
+    obs_lc = lineData[4]
+    obs_lat = lineData[6]
+    obs_lon = lineData[7]
+
+    #Print the location of sara
+    print(f"Record {record_id} indicates Sara was seen at lat:{obs_lat},lon:{obs_lon} on {obs_date}")
