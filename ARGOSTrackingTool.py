358224d32175403ff8ad2d22305808235eac4b98
emily.gy <emily.yang@duke.edu>
2025-10-14 20:23:09 -0400

    created date and location dictionaries

--- ARGOSTrackingTool.py
+++ ARGOSTrackingTool.py
@@ -21,35 +21,9 @@ line_list = file_object.readlines()
 #Close the file
 file_object.close()
 
-#Pretend we read one line of data from the file
-lineString = line_list[200]
-
-#Split the string into a list of data items
-lineData = lineString.split()
-
-#Extract items in list into variables
-record_id = lineData[0]
-obs_date = lineData[2]
-obs_lc = lineData[4]
-obs_lat = lineData[6]
-obs_lon = lineData[7]
-
-#Print the location of sara
-print(f"Record {record_id} indicates Sara was seen at lat:{obs_lat},lon:{obs_lon} on {obs_date}")
-
-
-# For loop
-#Create a variable pointing to the data file
-file_name = './data/raw/sara.txt'
-
-#Create a file object from the file
-file_object = open(file_name,'r')
-
-#Read contents of file into a list
-line_list = file_object.readlines()
-
-#Close the file
-file_object.close()
+# Initialize dictionaries 
+date_dict = {}
+location_dict = {}
 
 #Read contents of file into a list
 for lineString in line_list:
@@ -67,5 +41,9 @@ for lineString in line_list:
     obs_lat = lineData[6]
     obs_lon = lineData[7]
 
+    # Add items to dictionaries
+    date_dict[record_id] = obs_date
+    location_dict[record_id] = (obs_lat, obs_lon)
+
     #Print the location of sara
-    print(f"Record {record_id} indicates Sara was seen at lat:{obs_lat},lon:{obs_lon} on {obs_date}")
+    #print(f"Record {record_id} indicates Sara was seen at lat:{obs_lat},lon:{obs_lon} on {obs_date}")
